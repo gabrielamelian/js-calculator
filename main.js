d3cce15bbd96b70a807716a93e4c892bef8c2172
gabrielamelian <gabrielamelian@gmail.com>
2016-10-07 20:09:31 +1300

    adding new function logic, still not working 100 per cent

--- main.js
+++ main.js
@@ -19,30 +19,40 @@ function getButton(evt) {
         calculateResult();
     } else {
         equation += evt.target.value;
-        updateDOM("equation");
+        updateDOM("equation", equation);
     }
 }
 
+// function calculateResult() {
+//     var illegalExpression = /[\/\*][\/\*]/; // matches two characters: each is either * or /
+//     var twoPlusOrMinus = /(\+\+|\-\-)/ // matches two pluses together OR two minuses together
+//     var alsoIllegal = /[\+\-][\*\/]/; // matches two characters; first is + or -, second is * or /
+//     if ( (illegalExpression.test(equation) && equation.match(illegalExpression) != "**") || alsoIllegal.test(equation) ) {
+//         result = "Illegal!!";
+//     } else if (twoPlusOrMinus.test(equation)) {
+//         result = "Javascript don't like it";
+//     } else {
+//         result = eval(equation);
+//     }
+//     updateDOM("result", result);
+//     equation = result;
+// }
+
 function calculateResult() {
-    var illegalExpression = /[\/\*][\/\*]/; // matches two characters: each is either * or /
-    var twoPlusOrMinus = /(\+\+|\-\-)/ // matches two pluses together OR two minuses together
-    var alsoIllegal = /[\+\-][\*\/]/; // matches two characters; first is + or -, second is * or /
-    if ( (illegalExpression.test(equation) && equation.match(illegalExpression) != "**") || alsoIllegal.test(equation) ) {
-        result = "Illegal!!";
-    } else if (twoPlusOrMinus.test(equation)) {
-        result = "Javascript don't like it";
+    if (checkMalformedExpression() === true) {
+        updateDOM("result", "Malformed expression");
     } else {
         result = eval(equation);
+        updateDOM("result", result);
+        equation = result;
     }
-    updateDOM("result");
-    equation = result;
 }
 
 function clearResult() {
     result = 0;
     equation = "";
-    updateDOM("result");
-    updateDOM("zero");
+    updateDOM("result", result);
+    updateDOM(equation, "0");
 }
 
 function clearLast() {
@@ -58,20 +68,38 @@ function clearLast() {
   checkLastDigit();
 }
 
-function updateDOM(target) {
-    if (target === "equation") {
-        document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
-    } else if (target === "result") {
-        document.getElementById("result").innerHTML = "<p>" + result + "</p>";
-    } else if (target === "zero") {
-        document.getElementById("equation").innerHTML = "<p>0</p>";
-    }
+function updateDOM(target, content) {
+    document.getElementById(target).innerHTML = "<p>" + content + "</p>";
+    // if (target === "equation") {
+    //     document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
+    // } else if (target === "result") {
+    //     document.getElementById("result").innerHTML = "<p>" + result + "</p>";
+    // } else if (target === "zero") {
+    //     document.getElementById("equation").innerHTML = "<p>0</p>";
+    // }
 }
 
 function checkLastDigit() {
     if (equation.length === 0) {
-        updateDOM("zero");
+        updateDOM("equation", "0");
     } else {
-        updateDOM("equation");
+        updateDOM("equation", equation);
     }
+}
+
+function checkMalformedExpression () {
+    var operationArray = equation.split(/\d{1,}/g).filter(function(el) {return el.length != 0});
+    var isMalformed = false;
+    var allowedOperations = ["+-", "-+", "**", "*+", "*-", "/-", "/+", "+", "-", "*", "/"]
+    for (var i = 0; i < operationArray.length; i++) {
+        if (operationArray[i] != ()) {
+            isMalformed = true;
+            console.log(operationArray[i]);
+            console.log(isMalformed);
+            break;
+        } else {
+            isMalformed = false;
+        }
+    }
+    return isMalformed;
 }
